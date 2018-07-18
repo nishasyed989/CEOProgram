73688fc8181096890eaaf4a7ac738bec000048a5
nishasyed989 <nishasyed989@gmail.com>
2018-07-18 15:05:44 -0500

    adding file

---
+++ Strings.py
@@ -0,0 +1,20 @@
+"""
+title: Strings
+author: Nisha Seyed
+date: 7/18/18 2:56 PM
+"""
+?""
+greeting = "how's your day"
+
+
+
+def how_eligible(essay):
+        "return how many categories are in a string"
+
+
+
+
+
+if __name__ == '__main__':
+    print(how_eligible('This? "Yes." No, not really!'))
+    print(how_eligible('Really, not a compound sentence.'))
